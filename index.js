1190a9c6c83c6c7be9d7bd27e9cc6390ce118f0b
Dario Caruso <dev@dariocaruso.info>
2018-02-23 13:20:10 +0100

    seems ok

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Promise = require("bluebird");
+const getFolderSize = require("get-folder-size");
+const diskfree = require('diskfree');
+function getSizeInfo(path) {
+    return new Promise((resolve, reject) => {
+        getFolderSize(path, (err, size) => {
+            if (err)
+                return reject(err);
+            diskfree.check(path, (error, info) => {
+                if (error) {
+                    if (diskfree.isErrBadPath(err)) {
+                        reject(new Error('Path is Wrong'));
+                    }
+                    else if (diskfree.isErrDenied(error)) {
+                        reject(new Error('Permission Denied'));
+                    }
+                    else if (diskfree.isErrIO(error)) {
+                        reject(new Error('IO Error'));
+                    }
+                    else {
+                        reject(new Error('Unknown error: ' + error));
+                    }
+                }
+                else {
+                    info.size = size;
+                    info.percentUsed = parseInt(((size * 100) / info.total).toFixed(1));
+                    resolve(info);
+                }
+            });
+        });
+    });
+}
+exports.getSizeInfo = getSizeInfo;
---
+++ index.ts
@@ -0,0 +1,51 @@
+import * as Promise from "bluebird"
+
+const getFolderSize = require("get-folder-size")
+const diskfree = require('diskfree');
+
+export interface ISizeInfo {
+  available: number
+  free: number
+  total: number
+  used: number
+  locked: number
+  size: number
+  percentUsed: number
+}
+
+export function getSizeInfo(path: string): Promise<ISizeInfo> {
+
+  return new Promise<ISizeInfo>((resolve, reject) => {
+
+    getFolderSize(path, (err, size) => {
+      if (err) return reject(err)
+
+      diskfree.check(path, (error, info: ISizeInfo) => {
+        if (error) {
+          // You can see if its a known error 
+          if (diskfree.isErrBadPath(err)) {
+            reject(new Error('Path is Wrong'));
+          } else if (diskfree.isErrDenied(error)) {
+            reject(new Error('Permission Denied'))
+          } else if (diskfree.isErrIO(error)) {
+            reject(new Error('IO Error'))
+          } else {
+            reject(new Error('Unknown error: ' + error))
+          }
+
+        } else {
+          info.size = size
+          info.percentUsed = parseInt(((size * 100) / info.total).toFixed(1))
+          resolve(info)
+        }
+
+      });
+
+
+
+    })
+
+
+  })
+
+}
